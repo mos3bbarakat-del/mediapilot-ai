76a62dde3aec3b16f5f32107abc2ff0c67793853
mos3bbarakat-del <mos3b.barakat@gmail.com>
2026-09-25 21:49:01 +0300

    Use portable relative imports in home page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import Link from "next/link";
-import { AppShell } from "@/components/AppShell";
-import { useLocale } from "@/components/LocaleProvider";
+import { AppShell } from "../components/AppShell";
+import { useLocale } from "../components/LocaleProvider";
 
 export default function HomePage() {
   const { t } = useLocale();
